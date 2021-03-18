4adc6543ddd9f0c689e39f395127a1ddef01559f
Graph3x <80599181+Graph3x@users.noreply.github.com>
2021-03-18 20:17:06 +0100

    Deleted print()

--- server/generate_rsa.py
+++ server/generate_rsa.py
@@ -16,31 +16,3 @@ def generate_rsa_keys():
 		f.write(keys['pub'])
 
 	return {'priv':private_key, 'pub':public_key}
-
-
-def rsa_encrypt(key, data):
-	data = str.encode(data, 'utf-8')
-
-	with open(key, 'r') as f:
-		pub_key = f.read()
-
-	rsa_pub_key = RSA.importKey(pub_key)
-	rsa_pub_key = PKCS1_OAEP.new(rsa_pub_key)
-	encrypted_data = rsa_pub_key.encrypt(data)
-	return encrypted_data
-
-
-def rsa_decrypt(key, encrypted_data):
-	with open(key, 'r') as f:
-		priv_key = f.read()
-
-	rsa_priv_key = RSA.importKey(priv_key)
-	rsa_priv_key = PKCS1_OAEP.new(rsa_priv_key)
-	data = rsa_priv_key.decrypt(encrypted_data)
-	return data.decode('utf-8')
-
-
-enc = rsa_encrypt("public.key.pub","doud")
-print(enc)
-
-print(rsa_decrypt("private.key", enc))
